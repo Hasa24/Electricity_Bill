d20e3ff09a6152d6345900d9f74b0b147a4510f8
Hasa24 <109957706+Hasa24@users.noreply.github.com>
2026-01-12 20:37:53 +0530

    Refactor BillDetails class and improve database query

--- Electricity_Billing_System/src/electricity/billing/system/BillDetails.java
+++ Electricity_Billing_System/src/electricity/billing/system/BillDetails.java
@@ -5,31 +5,37 @@ import java.awt.*;
 import java.sql.*;
 import net.proteanit.sql.DbUtils;
 
-public class BillDetails extends JFrame{
+public class Bill extends JFrame {
 
-    BillDetails(String meter) {
-        
+    public Bill (String meterNo) {
+
+        setTitle("Bill Details");
         setSize(700, 650);
         setLocation(400, 150);
-        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         getContentPane().setBackground(Color.WHITE);
-        
-        JTable table = new JTable();
-        
+
+        JTable billTable = new JTable();
+        billTable.setRowHeight(25);
+
         try {
-            Conn c = new Conn();
-            String query = "select * from bill where meter_no = '"+meter+"'";
-            ResultSet rs = c.s.executeQuery(query);
-            
-            table.setModel(DbUtils.resultSetToTableModel(rs));
+            Conn conn = new Conn();
+            String query = "SELECT * FROM bill WHERE meter_no = ?";
+            PreparedStatement ps = conn.c.prepareStatement(query);
+            ps.setString(1, meterNo);
+
+            ResultSet rs = ps.executeQuery();
+            billTable.setModel(DbUtils.resultSetToTableModel(rs));
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
-        JScrollPane sp = new JScrollPane(table);
-        sp.setBounds(0, 0, 700, 650);
-        add(sp);
-        
+
+        JScrollPane scrollPane = new JScrollPane(billTable);
+        scrollPane.setBounds(0, 0, 700, 650);
+        add(scrollPane);
+
         setVisible(true);
     }
 
